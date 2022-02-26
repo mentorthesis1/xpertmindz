0d2ab2f961a06793f4954d4c67847cd57bab3d36
mentorthesis1 <php.mentorthesis@gmail.com>
2022-02-26 15:59:49 +0530

    about us changed

--- resources/views/layouts/BasePage.blade.php
+++ resources/views/layouts/BasePage.blade.php
@@ -215,11 +215,11 @@
           </div>
     
           <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+            <span class=" bi bi-chevron-left" aria-hidden="true"></span>
           </a>
     
           <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+            <span class=" bi bi-chevron-right" aria-hidden="true"></span>
           </a>
         </div>
     </section>     
@@ -380,39 +380,41 @@
    
 
     <!-- ======= About Us Section ======= -->
-    <section id="about" class="about" style="margin-top: -20px;">
+    <!-- End About Us Section -->
+    <section id="about" class="about" style="margin-top: -130px;">
       <div class="container" data-aos="fade-up">
+  
         <div class="section-title">
           <h2>About Us</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <p style="text-align: left; margin-top:20px;">Xpert Mindz -Innovative Solution private limited, is an educational institute under the Mentor Thesis, is administered by leading industrialists with Kuzhithurai as Head Quarters. It aims at<strong style="font-size:18px; font-style:italic;"> “Education For a Job”</strong>. The company committed to provide Quality Education in young minds of the budding engineers and also provides<strong style="font-size:18px; font-style:italic;"> Internship Courses , Workshops</strong>. This idea was in our mind for a long time, but took shape only in the year 2021.We provide services, such as <strong style="font-size:18px; font-style:italic;">Manpower , B.E/M.E projects</strong>.</hp>
+          <p style="text-align: left; margin-top:20px;">We also provide an excellent education system <strong style="font-size:18px; font-style:italic;">through academic, co-curricular and digital activities, education department, strategic trainings through skillful teachers,</strong> engaging deeply with all stakeholders, enhancing social-emotional learning, and carefully considering the metrics and goals wants to accomplish.</p>
+          <p style="text-align: left; margin-top:20px;">We impart standard technical education with excellence and relevance to the present and the contemporary society and empower development through <strong style="font-size:18px; font-style:italic;"> creative teaching, innovative thinking, developmental research, positive attitude building</strong>.</p>
         </div>
-      
+  
         <div class="row">
           <div class="col-lg-6" data-aos="fade-right">
-            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+           
+            <h4> <i class="bi bi-check-circle" style="color: blue"></i> MISSION</h4>
+           
+            <p>
+              The mission of the Department is to equip Candidate with <strong style="font-size:18px; font-style:italic;">specified knowledge, skills, and values to enable them to become good human beings and productive and to achieve excellence in whatever they do </strong>  and setting up regulatory and monitoring mechanism to streamline the functioning of works.
+            </p>
+           
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
-            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <ul>
-              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-            </ul>
+            
+            <h4> <i class="bi bi-check-circle" style="color: blue"></i> VISION</h4>
+            
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
+              To be a technical institution of high standard and quality comparable to global standards and to produce knowledgeable leaders and entrepreneurs of professional excellence with. Further committed to impart quality education and prepare students to achieve the highest level of intellectual excellence and to develop flair for research and make them enterprising engineers in the field of technical education.
             </p>
+            
+            
           </div>
         </div>
-
+  
       </div>
-    </section><!-- End About Us Section -->
-
+  </section>
     
     <!-- ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio">
